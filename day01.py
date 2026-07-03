7104debb8e6a9d132ddafb8841ad67bf1f71138e
Gunay Hacizade <ghacizade3@std.beu.az>
2026-07-03 21:27:49 +0400

    Day 1: variable, list, dict

--- day01.py
+++ day01.py
@@ -1,7 +1,7 @@
 name='Gunay'
 surname='Hacizade'
-age=20
-pi=3.14
+first=13
+second=19
 country='Azerbaijan'
 city='Baku'
 is_student=True
@@ -9,9 +9,10 @@ is_work=False
 nothing=None
 languages=['python','c','c++']
 me={'goal':'to be agentic ai engineer'}
-print(f"Hello, I am {name} {surname}, i am {age}. I am from {country} and i live in {city}")
+print(f"Hello, I am {name} {surname}, I am from {country} and i live in {city}")
 print(f"If you think that i am student, you are {is_student}, but if you think that i work, unfortunately you are {is_work}")
 print(f"but InshAllah i will work. Is there problem? {nothing}")
-print(f"When i was 13, i learned {languages[2]},when i was 19, i learned {languages[1]}.")
+print(f"When i was {first}, i learned {languages[2]},when i was {second}, i learned {languages[1]}.")
 print(f"But now i learn {languages[0]} to be agentic ai engineer.")
 print(f"And I have goal, this is {me['goal']}.")
+ex
