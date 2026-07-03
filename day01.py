040d8973fb0b4245e7635ff1a9cb43dd9dac3d62
Gunay Hacizade <ghacizade3@std.beu.az>
2026-07-03 20:13:41 +0400

    Day 1 complete: variables, list and dict

---
+++ day01.py
@@ -0,0 +1,17 @@
+name='Gunay'
+surname='Hacizade'
+age=20
+pi=3.14
+country='Azerbaijan'
+city='Baku'
+is_student=True
+is_work=False
+nothing=None
+languages=['python','c','c++']
+me={'goal':'to be agentic ai engineer'}
+print(f"Hello, I am {name} {surname}, i am {age}. I am from {country} and i live in {city}")
+print(f"If you think that i am student, you are {is_student}, but if you think that i work, unfortunately you are {is_work}")
+print(f"but InshAllah i will work. Is there problem? {nothing}")
+print(f"When i was 13, i learned {languages[2]},when i was 19, i learned {languages[1]}.")
+print(f"But now i learn {languages[0]} to be agentic ai engineer.")
+print(f"And I have goal, this is {me['goal']}.")
